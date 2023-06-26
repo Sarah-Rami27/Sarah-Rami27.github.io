2ca23647d0f904162699d01caba83c95fd1386ee
Sarah Ramirez <sarrami270@gmail.com>
2023-06-25 17:15:21 -0700

    added about me base

---
+++ components/AboutMe.js
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+import React from 'react';
+import {Row, Col, Container} from 'react-bootstrap';
+
+const AboutMe = () => {
+  return (
+    <>
+      <main className='bg-lightblue min-h-screen flex flex-col'>
+        <p className='text-5xl lg:text-6xl text-darkbrown font-serif-display text-center '>
+            About Me
+        </p>
+      </main>
+    </>
+  );
+};
+
+export default AboutMe;
--- components/Landing.js
+++ components/Landing.js
@@ -5,11 +5,11 @@ import {Row, Col, Container} from 'react-bootstrap';
 const Landing = () => {
   return (
     <>
-      <main className='bg-lightblue'>
+      <main className='bg-lightblue min-h-screen flex flex-col items-center justify-center'>
         <Container className='p-8'>
           <Row className='place-content-center'>
             <Col className='justify-center my-auto' xs={12} md={7} lg={8}>
-              <p className='text-6xl lg:text-9xl text-darkbrown font-serif-display md:text-left '>
+              <p className='text-6xl md:text-9xl text-darkbrown font-serif-display md:text-left'>
                 Sarah <br></br>Ramirez
               </p>
               <p className='text-2xl md:text-3xl text-darkbrownfont-roboto md:text-left'>
