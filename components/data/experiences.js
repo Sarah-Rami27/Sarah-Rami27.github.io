ef1256a6f59b961f876d7473e03377408d8a41b3
Sarah Ramirez <sarrami270@gmail.com>
2023-06-28 16:57:47 -0700

    Added Experience Page

---
+++ components/data/experiences.js
@@ -0,0 +1,36 @@
+const experiences = [
+  {
+    organization: "UCR Association for Computing Machinery",
+    role: "Web Developer",
+    location: "Riverside, CA",
+    duration: "July 2022 - June 2023",
+    description: [
+      "Work in a team of 10+ developers to develop a new, easy to navigate website for ACM at UCR",
+      "Utilized: Tailwind CSS, Next.js, React Bootstrap, Javascript, and Github",
+    ],
+  },
+  {
+    organization: "UCR Computer Science Department",
+    role: "Grader",
+    location: "Riverside, CA",
+    duration: "April 2022 - Present",
+    description: [
+      "Grade student's homeworks, exams, quizzes for the Discrete Structures course at UCR",
+      "Prepare solutions for homeworks, exams, and quizzes",
+      "Host quarterly LaTex workshops for students",
+    ],
+  },
+  {
+    organization: "iGen Connections",
+    role: "Creative",
+    location: "Riverside, CA",
+    duration: "February 2021 - Present",
+    description: [
+      "Tasked to create content for social media pages",
+      "Assist in brainstorming ideas for content that appeals to a wide range of viewers",
+      "Edit videos created for content purposes",
+    ],
+  },
+];
+
+export default experiences;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Landing from "../components/Landing";
 import AboutMe from "../components/AboutMe";
+import Experience from "../components/Experience";
 
 const Index = () => {
   return (
     <div className="overflow-y-auto overflow-x-hidden">
       <Landing />
       <AboutMe />
+      <Experience />
     </div>
   );
 };
