881e4f58525001291b0ab5786e1bc88bbedb7e90
Sarah Ramirez <sarrami270@gmail.com>
2024-07-12 13:53:31 -0700

    about me section

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -14,17 +14,18 @@ const AboutMe = () => {
           <Row className="place-content-center">
             <Col xs={12} lg={9}>
               <p className=" my-6 text-lg lg:text-xl xl:text-2xl text-darkbrown font-roboto text-left">
-                Hi, I'm Sarah Ramirez! I graduated with my Bachelors in
-                Science in Computer Science with Business Applications at the University of California, Riverside. I am
-                currently a Graduate student in Computer Science at the
-                University of California, Riverside. Alongside my academic
-                pursuits, I enjoy trying new foods, exploring new coffee shops, and
-                reading new books. I enjoy solving complex problems and
-                creating innovative solutions that can make a positive impact in
-                the world. Through my education and hands-on experience, I have
-                developed a solid foundation in various programming languages,
-                algorithms, and data structures. I can't wait to see what
-                opportunities come my way as continue in my educational career.
+                Hi, I'm Sarah Ramirez! I graduated with my Bachelors in Science
+                in Computer Science with Business Applications at the University
+                of California, Riverside. I am currently a Graduate student in
+                Computer Science at the University of California, Riverside.
+                Alongside my academic pursuits, I enjoy trying new foods,
+                exploring new coffee shops, and reading new books. I enjoy
+                solving complex problems and creating innovative solutions that
+                can make a positive impact in the world. Through my education
+                and hands-on experience, I have developed a solid foundation in
+                various programming languages, algorithms, and data structures.
+                I can't wait to see what opportunities come my way as continue
+                in my educational career.
               </p>
             </Col>
             <Col xs={7} lg={3} className="flex-col">
@@ -69,32 +70,28 @@ const AboutMe = () => {
                     Computer Science & Engineering:
                   </li>
                   <li className=" font-roboto leading-tight ">
-                      Advanced Computer Architecture
-                      &#x2022; Advanced Operating Systems
-                      &#x2022; Data Structures and Algorithms (including Design and Analysis of Algorithms)
-                      &#x2022; Discrete Structures
-                      &#x2022; Software Construction
-                      &#x2022; Computer Security
-                      &#x2022; Database Management Systems
-                      &#x2022; Computational Methods for Biomolecular Data
+                    Advanced Computer Architecture &#x2022; Advanced Operating
+                    Systems &#x2022; Data Structures and Algorithms (including
+                    Design and Analysis of Algorithms) &#x2022; Discrete
+                    Structures &#x2022; Software Construction &#x2022; Computer
+                    Security &#x2022; Database Management Systems &#x2022;
+                    Computational Methods for Biomolecular Data
                   </li>
                   <li className="text-lg font-bold leading-tight font-roboto text-left">
                     Data Science & Artificial Intelligence:
                   </li>
                   <li className="font-roboto leading-tight">
-                      Data Mining Techniques
-                      &#x2022; Machine Learning
-                      &#x2022; Artificial Intelligence
-                      &#x2022; Big Data Management
-                      &#x2022; Data Analysis Methods
-                      &#x2022; Natural Language Processing
+                    Data Mining Techniques &#x2022; Machine Learning &#x2022;
+                    Artificial Intelligence &#x2022; Big Data Management
+                    &#x2022; Data Analysis Methods &#x2022; Natural Language
+                    Processing
                   </li>
                   <li className="text-lg font-bold leading-tight font-roboto text-left">
                     Management & Decision Science:
                   </li>
                   <li className="font-roboto leading-tight">
-                      Decision Analysis and Management Science
-                      &#x2022; Production and Operations Management
+                    Decision Analysis and Management Science &#x2022; Production
+                    and Operations Management
                   </li>
                 </ul>
               </div>
@@ -110,7 +107,8 @@ const AboutMe = () => {
                   Technical Skills:
                 </p>
                 <p className="font-roboto text-left">
-                  C++, Javascript, Python, HTML/CSS, SQL, Pandas, NumPy, Scikit-Learn
+                  C++, Javascript, Python, HTML/CSS, SQL, Pandas, NumPy,
+                  Scikit-Learn
                 </p>
               </div>
               <div className="mt-6">
